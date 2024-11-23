c64446ef4aaea339db0b6fcae3b6a1acd7c30b24
vmarlier <valentin.marlier@pm.me>
2024-11-24 00:21:37 +0700

    feat: add /v1/tester endpoint

--- main.go
+++ main.go
@@ -3,19 +3,53 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-
 	addr := ":8080"
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("POST /v1/tester", testerHandler)
 
-	log.Infof("dummy-test is listening at %s...", addr)
+	log.Infof("dummy-tester is listening at %s...", addr)
 	log.Error(http.ListenAndServe(addr, mux))
+}
+
+func testerHandler(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		ExpectedStatus  int    `json:"expectedStatus"`
+		RequestDuration string `json:"requestDuration"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		log.WithFields(log.Fields{"err": err}).Error("Failed to decode request body")
+		return
+	}
+
+	// Log the received values
+	log.Infof("Received expectedStatus: %d, requestDuration: %s", body.ExpectedStatus, body.RequestDuration)
+
+	// Parse the request duration
+	duration, err := time.ParseDuration(body.RequestDuration)
+	if err != nil {
+		http.Error(w, "Invalid request duration", http.StatusBadRequest)
+		log.WithFields(log.Fields{"err": err}).Error("Failed to parse request duration")
+		return
+	}
+
+	// Wait for the specified duration
+	time.Sleep(duration)
+
+	// Return the expected status code
+	w.WriteHeader(body.ExpectedStatus)
+	w.Write([]byte("Request processed"))
+	log.Infoln("Request processed")
 }
 
 func healthHandler(w http.ResponseWriter, _ *http.Request) {
